1bea3d8cc0aa822ef890699f48467414414dc0ce
akash100a <arakash968786@gmail.com>
2025-06-23 22:14:53 +0600

    ok

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:"https://code.bdluminaries.com/api/v1/"
+  baseURL:"https://xyz.bdluminaries.com/api/v1/"
   // baseURL: "https://xyz.bdluminaries.com/api/v1/",
 });
 
